5c3ece9696c389088b719e45b5ed818c59bf4a58
divopsor <divopsor@gmail.com>
2023-08-10 17:35:57 +0900

    test [skip vercel] [skip vercel]

--- app/containers/Home/CategoryList.tsx
+++ app/containers/Home/CategoryList.tsx
@@ -1,3 +1,29 @@
+import { useRouter } from "next/navigation";
+import { Txt } from "../../components/ui/Txt";
+import { useCategoryList } from "../../hooks/useList";
+
+export function CategoryList() {
+  const [list] = useCategoryList();
+  const router = useRouter();
+
+  return (
+    <>
+      {
+        Object.entries(list ?? []).map(([key, value]) => (
+          <div style={{ cursor: 'pointer' }} key={key} onClick={() => {
+            router.push(`/?category=${value}`);
+          }}>
+            <Txt style={{ textAlign: 'center', fontSize: '2.4rem', fontWeight: 'bold' }}>
+              {`👉 ${key} 보러가기`}
+            </Txt>
+          </div>
+        ))
+      }
+    </>
+  )
+}
+
+/**
 import Link from "next/link";
 import { Txt } from "../../components/ui/Txt";
 import { useCategoryList } from "../../hooks/useList";
@@ -19,3 +45,4 @@ export function CategoryList() {
     </>
   )
 }
+ */
