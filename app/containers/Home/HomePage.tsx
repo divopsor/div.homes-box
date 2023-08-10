f7eb873486fd12fc2dd6776bfc9576d873dc2482
divopsor <divopsor@gmail.com>
2023-08-10 19:38:13 +0900

    isMounted [skip vercel]

--- app/containers/Home/HomePage.tsx
+++ app/containers/Home/HomePage.tsx
@@ -5,22 +5,27 @@ import { Suspense, useEffect } from 'react';
 import { Container } from '../../components/ui/Container';
 import { Spacing } from '../../components/ui/Space';
 import { Stack } from '../../components/ui/Stack';
+import { useIsMounted } from '../../hooks/useIsMounted';
 import { useCategoryList } from '../../hooks/useList';
 import { Form } from './Form';
 import { List } from './List';
-import { Sidebar } from './Sidebar';
 
 export const HomePage = () => {
+  const isMounted = useIsMounted();
   const [list] = useCategoryList();
   const searchParams = useSearchParams();
   const category = searchParams.get('category')!;
   const router = useRouter();
 
   useEffect(() => {
+    if (!isMounted) {
+      return;
+    }
+
     if (category == null || category === '' || category === 'undefined') {
       router.push(`/?category=${Object.values(list)[0]}`);
     }
-  }, [category, list])
+  }, [isMounted, category, list])
 
   return (
     <main>
