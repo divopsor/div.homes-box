d94437d3d2687ebd55a454b74a9ac3d0c29cafa6
divopsor <divopsor@gmail.com>
2023-12-28 14:22:05 +0900

    remove emotion [skip vercel]

--- app/hooks/useMetaKeyShortcut.ts
+++ app/hooks/useMetaKeyShortcut.ts
@@ -3,6 +3,26 @@ import { useEffect } from "react";
 export function useMetaKeyShortcut(keys: Record<string, () => void>, cacheKeys: any[] = []) {
   useEffect(() => {
     const eventHandler = (event: KeyboardEvent) => {
+      console.log((event));
+      navigator.clipboard
+      .read()
+      .then(
+        async (clipboardContents) => {
+          console.log(clipboardContents);
+          for (const item of clipboardContents) {
+            console.log(item.types);
+            if (!item.types.includes("image/png")) {
+              return;
+            }
+            console.log(1);
+            const blob = await item.getType("image/png");
+            console.log(2);
+            console.log(URL.createObjectURL(blob));
+          }
+        }
+      ).catch(
+        (error) => console.error(error.message)
+      );
       if (
         Object.keys(keys).includes(`${event.key}`) && 
         event.metaKey &&
